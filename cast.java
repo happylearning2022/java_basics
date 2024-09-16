4ee9014357dcbc60f43fae806ec85912aa0ccda6
vjsmart <happylearning2022@gmail.com>
2024-09-16 12:33:49 -0400

    added cast practiced java file

---
+++ cast.java
@@ -0,0 +1,21 @@
+class Cast{
+
+public static void main(String[]args)
+{
+double d = 1234555;
+System.out.println("d = "+d);
+
+double d1 = 'p';
+System.out.println("d1 = "+d1);
+
+int i1 = 'p';
+System.out.println("i1 = "+i1);
+
+char c =(char)5;
+System.out.println("c = "+c);
+
+int i = (int)9.444;
+System.out.println("i = "+i);
+}
+
+}
